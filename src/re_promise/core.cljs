397cb4537a7292e35ab5a99e69d63cf427f15632
Oskar Zabik <oskar.zabik@gmail.com>
2019-03-30 13:21:04 +0100

    Add implementation for `*-n` calls

--- src/re_promise/core.cljs
+++ src/re_promise/core.cljs
@@ -1,10 +1,29 @@
 (ns re-promise.core
   (:require [re-frame.core :refer [reg-fx dispatch]]))
 
-(reg-fx :promise
-  (fn [{:keys [call on-failure on-success]}]
-    (-> (call)
-        (.then (fn [response]
-                 (dispatch (conj on-success response))))
-        (.catch (fn [err]
-                  (dispatch (conj on-failure err)))))))
+(defn- dispatch-conj [handler data]
+  (when handler
+    (dispatch (conj handler data))))
+
+(defn handle-re-promise
+  [{:keys [call on-failure on-success on-failure-n on-success-n] :as pr}]
+  (-> (call)
+      (.then (fn [response]
+               (dispatch-conj on-success response)
+               (doseq [handler on-success-n]
+                 (dispatch-conj handler response))))
+      (.catch (fn [err]
+                (dispatch-conj on-failure err)
+                (doseq [handler on-failure-n]
+                  (dispatch-conj handler err))
+                (when (not (or on-failure on-failure-n))
+                  (throw err))))))
+
+(reg-fx :promise handle-re-promise)
+
+(reg-fx :promise-n
+  (fn [re-promises]
+    (doseq [re-promise re-promises]
+      (prn re-promise))
+    (doseq [re-promise re-promises]
+      (handle-re-promise re-promise))))
